a8872809a0d31493a0db5ac51fa311c5c8d9cbea
Om Sharma <omsharma.83173@gmail.com>
2025-04-15 17:25:21 +0530

    adder-route-for-uploading-csv-and-processing-it!

--- Bidirectional-ClickHouse-&-Flat-File-Data/backend/src/routes/uploadCsv.ts
+++ Bidirectional-ClickHouse-&-Flat-File-Data/backend/src/routes/uploadCsv.ts
@@ -1,14 +1,39 @@
 import express from "express";
 import {Request,Response} from "express"
+import multer from "multer";
+import fs from "fs";
+import csvParser from "csv-parser";
+import { error } from "console";
 
 const router = express.Router();
 
-
+const upload = multer({dest:'uploads/'});
 
 router.get('/dd',(req:Request,res:Response)=>{
     res.json({msg:"hiiiii"});
     return;
 })
 
+
+router.post('/upload-csv',upload.single('file'),async(req:Request,res:Response)=>{
+    const filePath = req.file?.path;
+    const rows : any[] = [];
+    if(!filePath){
+        res.status(400).json({error:"No file Uploaded"})
+        return;
+    }
+
+    // parsing the csv format..
+    fs.createReadStream(filePath).pipe(csvParser()).on('data',(data)=>{rows.push(data)}).on('end',()=>{
+        res.json({
+            message:"CSV Uploded and Parsed!",
+            rowCount:rows.length,
+            preview:rows.slice(0,5)
+        })
+    })
+
+    return;
+
+})
 
 export default router;
