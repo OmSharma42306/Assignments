bca45c7afd309e55f4f3a9ee9999a4a15abf9408
Om Sharma <omsharma.83173@gmail.com>
2025-04-15 18:16:30 +0530

    removed-unnecessary-folders

--- Bidirectional-ClickHouse-&-Flat-File-Data/backend/src/api/api.ts
+++
@@ -1,9 +0,0 @@
-import express from "express";
-import csvRouter from "../routes/uploadCsv"
-
-const router = express.Router();
-
-router.use('/data',csvRouter)
-
-
-export default router;
--- Bidirectional-ClickHouse-&-Flat-File-Data/readme.md
+++
@@ -1,3 +0,0 @@
-# Assignment For Bidirectional ClickHouse & Flat File Data Ingestion Tool
-
-# TECH STACK : MERN STACK
