4bc043f19f2873c27f24ca3d5848444b745a0919
Om Sharma <omsharma.83173@gmail.com>
2025-04-15 16:53:57 +0530

    created-a-client-and-fetching-data-from-clickHouse

--- Bidirectional-ClickHouse-&-Flat-File-Data/backend/src/index.ts
+++ Bidirectional-ClickHouse-&-Flat-File-Data/backend/src/index.ts
@@ -1,6 +1,11 @@
 import express from "express";
 import cors from "cors";
 import bodyParser from "body-parser";
+import { Request,Response } from "express";
+import createClientClickHouse from "./clickhouseClient";
+import dotenv from 'dotenv'
+dotenv.config();
+
 const PORT = 3000;
 
 const app = express();
@@ -10,9 +15,23 @@ app.use(cors());
 
 
 
+app.get('/',async (req:Request,res:Response)=>{
 
+    const client = await createClientClickHouse();
+
+    const resultSet = await client.query({
+        query: 'SELECT * from xyz',
+        format: 'JSONEachRow',
+      })
+      
+    const rows = await resultSet.json() ;
+    console.log(rows)
+
+    res.json({msg:"Backend is UP!",rows});
+    return;
+})
 
 
 app.listen(PORT,()=>{
     console.log("Server Started at PORT:",PORT);
-})
+});
